7ee3595d9526f3632ed9490058a9081e7bc2d441
ZT229 <2432702069@qq.com>
2025-04-05 16:00:55 +0800

    移除filetransfer插件并修改ath11k源码来源

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -3,6 +3,7 @@ BOARDNAME:=Qualcomm Atheros IPQ60xx
 DEFAULT_PACKAGES += ath11k-firmware-ipq6018 nss-firmware-ipq6018
 TARGET_CFLAGS += -O3 -pipe -funroll-loops -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53
 TARGET_LDFLAGS += -Wl,-O3
+DEFAULT_PROFILE:=xiaomi_ax1800
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ60xx based boards.
 endef
